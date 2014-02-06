62fc68b11a28ad9125d1becb1cb773c472bee548
root <root@pulse2-fai.metz.mandriva.com>
2014-02-06 10:36:13 +0100

    tcp_proxy script

---
+++ proxy/tcp_proxy.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+from twisted.internet import defer
+from twisted.internet import protocol
+from twisted.internet import reactor
+from twisted.python import log
+
+# Proxy params
+if len(sys.argv) < 2:
+    print 'Usage : tcp_proxy local_port:destination_host:destination_port'
+    sys.exit(1)
+
+if len(sys.argv[1].split(':')) != 3:
+    print 'Argument error'
+
+(from_port, dest_host, dest_port) = sys.argv[1].split(':')
+
+try:
+    from_port = int(from_port)
+    dest_port = int(dest_port)
+except:
+    print 'Argument error'
+
+#from_port = 6868
+#dest_port = 6666
+#dest_host = '192.168.71.208'
+
+class ProxyClientProtocol(protocol.Protocol):
+    def connectionMade(self):
+        log.msg("Client: connected to peer")
+        self.cli_queue = self.factory.cli_queue
+        self.cli_queue.get().addCallback(self.serverDataReceived)
+
+    def serverDataReceived(self, chunk):
+        if chunk is False:
+            self.cli_queue = None
+            log.msg("Client: disconnecting from peer")
+            self.factory.continueTrying = False
+            self.transport.loseConnection()
+        elif self.cli_queue:
+            log.msg("Client: writing %d bytes to peer" % len(chunk))
+            self.transport.write(chunk)
+            self.cli_queue.get().addCallback(self.serverDataReceived)
+        else:
+            self.factory.cli_queue.put(chunk)
+
+    def dataReceived(self, chunk):
+        log.msg("Client: %d bytes received from peer" % len(chunk))
+        self.factory.srv_queue.put(chunk)
+
+    def connectionLost(self, why):
+        if self.cli_queue:
+            self.cli_queue = None
+            log.msg("Client: peer disconnected unexpectedly")
+
+
+class ProxyClientFactory(protocol.ReconnectingClientFactory):
+    maxDelay = 10
+    continueTrying = True
+    protocol = ProxyClientProtocol
+
+    def __init__(self, srv_queue, cli_queue):
+        self.srv_queue = srv_queue
+        self.cli_queue = cli_queue
+
+class ProxyServer(protocol.Protocol):
+    def connectionMade(self):
+        self.srv_queue = defer.DeferredQueue()
+        self.cli_queue = defer.DeferredQueue()
+        self.srv_queue.get().addCallback(self.clientDataReceived)
+
+        factory = ProxyClientFactory(self.srv_queue, self.cli_queue)
+        reactor.connectTCP(dest_host, dest_port, factory)
+
+    def clientDataReceived(self, chunk):
+        log.msg("Server: writing %d bytes to original client" % len(chunk))
+        self.transport.write(chunk)
+        self.srv_queue.get().addCallback(self.clientDataReceived)
+
+    def dataReceived(self, chunk):
+        log.msg("Server: %d bytes received" % len(chunk))
+        self.cli_queue.put(chunk)
+
+    def connectionLost(self, why):
+        self.cli_queue.put(False)
+
+if __name__ == "__main__":
+    log.startLogging(sys.stdout)
+    factory = protocol.Factory()
+    factory.protocol = ProxyServer
+    reactor.listenTCP(from_port, factory) #, interface="0.0.0.0")
+    reactor.run()
